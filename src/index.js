9dd5680ded69c542f253272283c81b842d00008d
trentspi <tspice23@att.net>
2017-05-23 15:54:47 -0400

    Added header component + styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import App from './js/App';
+import registerServiceWorker from './js/registerServiceWorker';
+import './css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
