7474f774315bdce089d2484063ddf28fa8129da2
trentspi <tspice23@att.net>
2017-05-24 16:12:27 -0400

    Added example firebase key

---
+++ src/js/base.example.js
@@ -0,0 +1,15 @@
+import rebase from 're-base';
+import firebase from 'firebase/app';
+import database from 'firebase/database';
+
+const app = firebase.initializeApp({
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+});
+
+const db = database(app);
+export default rebase.createClass(db);
